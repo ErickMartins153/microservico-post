bc5cc984bf4d033ad34b4268068d52c233aeca87
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2024-11-20 23:35:46 -0300

    feat: adicionar tratam

---
+++ src/main/java/br/upe/base/repositories/PostRepository.java
@@ -0,0 +1,10 @@
+package br.upe.base.repositories;
+
+import br.upe.base.models.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.UUID;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, UUID> {
+}
