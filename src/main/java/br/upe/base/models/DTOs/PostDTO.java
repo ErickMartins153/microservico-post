b228ad71c42f65e63540da274addb1cb2fed3c67
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2024-11-21 10:34:16 -0300

    fix: arrumar lógica de Post

--- src/main/java/br/upe/base/models/DTOs/PostDTO.java
+++ src/main/java/br/upe/base/models/DTOs/PostDTO.java
@@ -1,5 +1,6 @@
 package br.upe.base.models.DTOs;
 
+import br.upe.base.models.Post;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.Instant;
@@ -15,4 +16,34 @@ public record PostDTO(
         Set<String> hashTags,
         Instant dataPublicacao
 ) {
+        public void addHashTag(String hashTag) {
+                hashTags.add(hashTag);
+        }
+
+        public void deleteHashTag(String hashTag) {
+                hashTags.remove(hashTag);
+        }
+
+        public static PostDTO from(Post post) {
+                return new PostDTO(
+                        post.getId(),
+                        post.getTitulo(),
+                        post.getConteudo(),
+                        post.getCurtidas(),
+                        post.getHashTags(),
+                        post.getDataPublicacao()
+                );
+        }
+
+        // Método estático para converter de PostDTO para Post
+        public static Post to(PostDTO postDTO) {
+                Post post = new Post();
+                post.setId(postDTO.id());
+                post.setTitulo(postDTO.titulo());
+                post.setConteudo(postDTO.conteudo());
+                post.setCurtidas(postDTO.curtidas());
+                post.setHashTags(postDTO.hashTags());
+                post.setDataPublicacao(postDTO.dataPublicacao());
+                return post;
+        }
 }
